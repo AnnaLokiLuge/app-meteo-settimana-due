0d76f90793d48b7b30ae74edf7b5f7f48799abbb
Anna Bonomo <annaaracnophobia@gmail.com>
2022-09-09 10:59:06 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import Meteo from './components/Meteo';
+function App() {
+  return (
+    <div className="container-fluid">
+     
+          <h1 className="m-3 p-3 text-center shadow-lg ">App Meteo</h1>
+          <Meteo />
+        </div>
+  );
+}
+
+export default App;
